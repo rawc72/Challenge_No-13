3779fe6733a90c22642f03638e9d9ec78ae4ac03
Robert Anthony Williams <rawc@me.com>
2022-01-11 08:14:15 -0500

    Executable file and .json file.

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const { Category, Product, ProductTag, Tag } = require("./models");
+
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+const syncAll = async () => {
+  await sequelize.sync({ force: true });
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+syncAll();
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
